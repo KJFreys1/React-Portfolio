3cd1501d19eb7fc79a67ce7d2b55b2608899e75b
KJFreys1 <kfreyermuth20@gmail.com>
2020-02-24 15:37:52 -0800

    modified project info

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,25 +5,35 @@ export default class Projects extends React.Component {
         return (
             <section className='box flex'>
                 <div className='proj-title'>
+                    <h2 className='proj'><a className='highlight' href='https://wonderful-almeida-16ee2b.netlify.com'>DevSync</a></h2>
+                </div>
+                <div className='proj-desc'>
+                    <h2 className='proj'>MERN Stack with Socket.io</h2>
+                    <p className='desc'>Asynchronous chatroom with user authorization and web sockets</p>
+                </div>
+
+                <div className='proj-title'>
                     <h2 className='proj'><a className='highlight' href='https://kjfreys1.github.io/Arcade/'>Arcade</a></h2>
                 </div>
                 <div className='proj-desc'>
                     <h2 className='proj'>JavaScript DOM Manipulation Games</h2>
                     <p className='desc'>Simon memory game, Snake, and NES-style Bomberman</p>
                 </div>
+
                 <div className='proj-title'>
-                    <h2 className='proj'><a className='highlight' href='http://stock-lab-kylefreyermuth.surge.sh'>Stocks</a></h2>
+                    <h2 className='proj'><a className='highlight' href='https://clever-bardeen-11e12a.netlify.com/'>Thought Shower</a></h2>
                 </div>
                 <div className='proj-desc'>
-                    <h2 className='proj'>JavaScript with React and APIs</h2>
-                    <p className='desc'>Tracks current popular stocks in real-time</p>
+                    <h2 className='proj'>MERN Stack</h2>
+                    <p className='desc'>Social media mock for developers using Bootstrap CSS</p>
                 </div>
+
                 <div className='proj-title'>
-                    <h2 className='proj'><a className='highlight' href='https://upbeat-lalande-3f5ac0.netlify.com/blog/Introduction'>Blog</a></h2>
+                    <h2 className='proj'><a className='highlight' href='https://gatodolist.herokuapp.com/'>ToDo List</a></h2>
                 </div>
                 <div className='proj-desc'>
-                    <h2 className='proj'>React with JSON</h2>
-                    <p className='desc'>Displays more information about projects</p>
+                    <h2 className='proj'>Django Views and Python</h2>
+                    <p className='desc'>Basic to-do list using Django, PostgreSQL and Python</p>
                 </div>
             </section>
         )
