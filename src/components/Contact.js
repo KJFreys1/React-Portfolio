c8839cc0982d6e1d455f02aa6f76d557365b921b
KJFreys1 <kfreyermuth20@gmail.com>
2020-02-24 16:47:44 -0800

    updated email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ export default class Nav extends React.Component {
         return (
             <footer>
                 <div className='foot-link'>(760) 822-1694</div>
-                <div className='foot-link'>kfreyermuth20@gmail.com</div>
+                <div className='foot-link'>kylefrey.dev@gmail.com</div>
                 <a className='foot-link highlight' href='https://github.com/KJFreys1'>GitHub</a>
                 <a className='foot-link highlight' href='https://www.linkedin.com/in/kylefreyermuth/'>LinkedIn</a>
             </footer>
